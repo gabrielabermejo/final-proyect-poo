43ced3a22c0dfa7749df9c19b71137d8bf863844
gabrielabermejo <98917590+gabrielabermejo@users.noreply.github.com>
2022-04-21 03:19:58 -1100

    Update proyect.py

--- proyect.py
+++ proyect.py
@@ -3,4 +3,8 @@ class appfit:
     self.peso = peso
     self.altura = altura
     self.edad = edad
-    self.fitapagado = Falso
+    self.fitapagado = False
+usuario = appfit("50kg", "1.60", "15")
+print(type(usuario))
+
+print(usuario.peso, usuario.altura, usuario.edad)
